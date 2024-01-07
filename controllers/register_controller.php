526f8274f6465253cd03a2b1a5516e851ee9a290
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-07 16:23:17 +0100

    catch the exception on the signup

--- controllers/register_controller.php
+++ controllers/register_controller.php
@@ -7,16 +7,19 @@ if (isset($_POST['signup'])) {
     $targetDir = "assets/img/";
     $fileName = basename($_FILES["picture"]["name"]);
     $targetFilePath = $targetDir . $fileName;
-
-    // Upload file to server
+    
     if (move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFilePath)) {
 
         $userChecker = User::user_checker($email, $db);
-        if ($userChecker) {
-            throw new Exception("User_exist");
-        } else {
-            User::insertUser($username, $email, $password, $fileName, $db);
-            header('Location: index.php?page=login');
+        try {
+            if ($userChecker) {
+                throw new Exception("User_exist");
+            } else {
+                User::insertUser($username, $email, $password, $fileName, $db);
+                header('Location: index.php?page=login');
+            }
+        } catch (Exception $e) {
+            header("Location: index.php?page=register&error=" . $e->getMessage());
         }
     }
 }
