aee9c4d253e2617383d57f79c61a1fca40544cdc
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-07 16:23:00 +0100

    catch the exception on the login

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -3,13 +3,20 @@
 if (isset($_POST['login'])) {
     extract($_POST);
     $userChecker = User::user_checker($email, $db);
-    if ($userChecker) {
-        if (password_verify(trim($password), $userChecker["password"])) {
-            $authentication = new Authentication();
-            $authentication->login($userChecker["user_id"]);
-        } else
-            throw new Exception("password_incorrect");
-    } else {
-        throw new Exception("User_doesnt_exist");
+
+    try {
+        if ($userChecker) {
+            if (password_verify(trim($password), $userChecker["password"])) {
+                $authentication = new Authentication();
+                $authentication->login($userChecker["user_id"]);
+            } else {
+                throw new Exception("password_incorrect");
+            }
+        } else {
+            throw new Exception("User_doesnt_exist");
+        }
+    } catch (Exception $e) {
+        // Handle the exception here
+        header("Location: index.php?page=login&error=" . $e->getMessage());
     }
 }
