a1153b8403f690ec9633bf0217b3531658640bce
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-07 16:04:37 +0100

    add authentication class

---
+++ _classes/Authentication.php
@@ -0,0 +1,14 @@
+<?php
+
+class Authentication {
+    function login ($user_id) {
+        $_SESSION["user_id"] = $user_id;
+        $_SESSION["login"] = true;
+        header('Location: index.php?page=home');
+    }
+
+    function logout () {
+        session_destroy();
+        header('Location: index.php');
+    }
+}
