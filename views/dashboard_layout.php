11ad264fd0a9b4391f73cab91f2f791208f78145
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-08 12:14:59 +0100

    add dashboard layout file

---
+++ views/dashboard_layout.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= ucfirst($page) ?></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"/>
+</head>
+<body>
+<!-- header ends here -->
+
+<?php include_once 'views/' . $page . '_view.php'; ?>
+
+
+<script src="<?= PATH ?>assets/js/main.js"></script>
+</body>
+</html>
