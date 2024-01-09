7ce89f22794c36e2027df9460de28dc6e6dcea25
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:16:30 +0100

    get tags using ajax

--- assets/js/tag.js
+++ assets/js/tag.js
@@ -24,3 +24,46 @@ addTagBtn.addEventListener("click", (event) => {
     } else
         addTagInput.style.border = "1px solid red";
 })
+
+function getTags() {
+    tagSection.innerHTML = "";
+    $.get(
+        "index.php?page=dashboard&get_tags=true",
+        (data) => {
+            console.log(data);
+            let tags = JSON.parse(data);
+
+            tags.forEach((tag) => {
+                tagSection.innerHTML += `<tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">${tag.tag_id}</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">${tag.tag}</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                <a href="#tag-form" class="edit-tag text-blue-600 dark:text-blue-500 hover:underline"
+                                                data-tag-id="${tag.tag_id}" data-tag-name="${tag.tag}">Edit</a>
+                                            </td>
+                                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                <a data-tag-id="${tag.tag_id}" class="delete-tag cursor-pointer text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                                            </td>
+                                        </tr>`;
+            })
+        }
+    )
+
+    $(tagSection).ready(() => {
+        $(tagSection).on("click", ".edit-tag", function () {
+            currentTagId = $(this).data("tag-id");
+            currentTag = $(this).data("tag-name");
+            console.log(document.querySelector("#edit-tag-form").classList);
+            document.querySelector("#edit-tag-form").classList.remove("hidden");
+            document.querySelector("#tag-form").classList.add("hidden");
+            document.querySelector("#edit-tag-input").value = currentTag;
+        })
+
+        $(tagSection).on("click", ".delete-tag", function () {
+            currentTagId = $(this).data("tag-id");
+            deleteTag(currentTagId);
+        });
+    })
+
+
+}
