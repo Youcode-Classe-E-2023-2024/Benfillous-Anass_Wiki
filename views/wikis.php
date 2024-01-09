9c2eaa7923fcaea02a73cec2084f9cc29e4cfd4c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 00:24:41 +0100

    update wikis view

--- views/wikis.php
+++ views/wikis.php
@@ -5,101 +5,22 @@
         <div class="absolute -bottom-32 left-20 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         <div id="wikis-container" class="m-8 relative space-y-4">
             <div class="flex justify-end">
-            <button id="add-wiki-btn" class="w-1/4 bg-gray-300 rounded-sm hover:bg-gray-200">Add New Wiki</button>
+                <button id="add-wiki-btn" class="w-1/4 bg-gray-300 rounded-sm hover:bg-gray-200">Add New Wiki</button>
             </div>
             <table class="w-full bg-white rounded-lg">
                 <thead>
-                    <tr class="p-5 bg-gray-300">
-                        <th class="h-6 w-10 text-center rounded">Id</th>
-                        <th class="h-6 w-32 text-center rounded">Title</th>
-                        <th class="w-32 h-6 text-center rounded-lg">Description</th>
-                        <th class="w-24 h-6 text-center rounded-lg">Creator</th>
-                        <th class="w-24 h-6 text-center rounded-lg">Tags</th>
-                        <th class="w-24 h-6 text-center rounded-lg">Category</th>
-                        <th class="w-24 h-6 text-center rounded-lg">Created</th>
-                        <th class="w-24 h-6 text-center rounded-lg">Action</th>
-                    </tr>
+                <tr class="p-5 bg-gray-300">
+                    <th class="h-6 w-10 text-center rounded">Id</th>
+                    <th class="h-6 w-32 text-center rounded">Title</th>
+                    <th class="w-32 h-6 text-center rounded-lg">Description</th>
+                    <th class="w-24 h-6 text-center rounded-lg">Creator</th>
+                    <th class="w-24 h-6 text-center rounded-lg">Tags</th>
+                    <th class="w-24 h-6 text-center rounded-lg">Category</th>
+                    <th class="w-24 h-6 text-center rounded-lg">Created</th>
+                    <th class="w-24 h-6 text-center rounded-lg">Action</th>
+                </tr>
                 </thead>
-                <tbody>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr class="p-6">
-                        <td class="h-6 w-10 text-center rounded">1</td>
-                        <td class="h-6 w-32 text-center rounded">Example 1</td>
-                        <td class="w-32 h-6 text-center rounded-lg">Example Description</td>
-                        <td class="w-24 h-6 text-center rounded-lg">John Doe</td>
-                        <td class="w-24 h-6 text-center rounded-lg">tagtogi</td>
-                        <td class="w-24 h-6 text-center rounded-lg">cateorza</td>
-                        <td class="w-24 h-6 text-center rounded-lg">Lbare7</td>
-                        <td class="w-24 h-6 text-center rounded-lg">
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-
+                <tbody id="wikis-list">
                 </tbody>
             </table>
         </div>
@@ -117,28 +38,33 @@
                 <!--Card-->
                 <div id='section2' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
 
-                    <form>
+                    <form id="wiki-form">
 
                         <div class="md:flex mb-6">
                             <div class="md:w-1/3">
-                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textfield">
+                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4"
+                                       for="my-textfield">
                                     Text Field
                                 </label>
                             </div>
                             <div class="md:w-2/3">
-                                <input placeholder="Write Title here" class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="">
+                                <input placeholder="Write Title here" class="form-input block w-full focus:bg-white"
+                                       id="title-input" type="text" value="">
                             </div>
                         </div>
 
                         <div class="md:flex mb-6">
                             <div class="md:w-1/3">
-                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textarea">
+                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4"
+                                       for="my-textarea">
                                     Category
                                 </label>
                             </div>
                             <div class="md:w-2/3">
-                                <select name="" class="form-select block w-full focus:bg-white" id="my-select">
-                                    <option value="A">A</option>
+                                <select name="" class="form-select block w-full focus:bg-white" id="category-input">
+                                    <?php foreach ($categories as $category) { ?>
+                                        <option value="<?= $category["category_id"] ?>"><?= $category["category"] ?></option>
+                                    <?php } ?>
                                 </select>
 
                                 <p class="py-2 text-sm text-gray-600">Chose the category</p>
@@ -147,38 +73,41 @@
 
                         <div class="md:flex mb-6">
                             <div class="md:w-1/3">
-                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-multiselect">
+                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4"
+                                       for="my-multiselect">
                                     Select Tag
                                 </label>
                             </div>
                             <div class="md:w-2/3">
-                                <select class="form-multiselect block w-full" multiple id="my-multiselect">
-                                    <option>Tag 1</option>
-                                    <option>Tag 2</option>
-                                    <option>Tag 3</option>
-                                    <option>Tag 4</option>
-                                    <option>Tag 5</option>
+                                <select class="form-multiselect block w-full" multiple id="tag-input">
+                                    <?php foreach ($tags as $tag) { ?>
+                                        <option value="<?= $tag["tag_id"] ?>"><?= $tag["tag"] ?></option>
+                                    <?php } ?>
                                 </select>
-                                <p class="py-2 text-sm text-gray-600">add notes about populating the field</p>
+                                <p class="py-2 text-sm text-gray-600">Chose tags</p>
                             </div>
                         </div>
 
                         <div class="md:flex mb-6">
                             <div class="md:w-1/3">
-                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textarea">
+                                <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4"
+                                       for="my-textarea">
                                     Content
                                 </label>
                             </div>
                             <div class="md:w-2/3">
-                                <textarea placeholder="Write The Content Here" class="form-textarea block w-full focus:bg-white" id="my-textarea" value="" rows="8"></textarea>
+                                <textarea placeholder="Write The Content Here"
+                                          class="form-textarea block w-full focus:bg-white" id="content-input" value=""
+                                          rows="8"></textarea>
                                 <p class="py-2 text-sm text-gray-600">Add ore details about your Wiki</p>
                             </div>
                         </div>
 
                         <div class="md:flex md:items-center">
-                                <button class="shadow bg-yellow-700 hover:bg-yellow-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                                    Save
-                                </button>
+                            <button class="shadow bg-yellow-700 hover:bg-yellow-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                                    type="button" id="wiki-submit">
+                                Save
+                            </button>
                         </div>
                     </form>
 
@@ -202,20 +131,22 @@
             // Cache selectors
             var lastId,
                 topMenu = $("#menu-content"),
-                topMenuHeight = topMenu.outerHeight()+175,
+                topMenuHeight = topMenu.outerHeight() + 175,
                 // All list items
                 menuItems = topMenu.find("a"),
                 // Anchors corresponding to menu items
-                scrollItems = menuItems.map(function(){
+                scrollItems = menuItems.map(function () {
                     var item = $($(this).attr("href"));
-                    if (item.length) { return item; }
+                    if (item.length) {
+                        return item;
+                    }
                 });
 
             // Bind click handler to menu items
             // so we can get a fancy scroll animation
-            menuItems.click(function(e){
+            menuItems.click(function (e) {
                 var href = $(this).attr("href"),
-                    offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+                    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
                 $('html, body').stop().animate({
                     scrollTop: offsetTop
                 }, 300);
@@ -226,17 +157,17 @@
             });
 
             // Bind to scroll
-            $(window).scroll(function(){
+            $(window).scroll(function () {
                 // Get container scroll position
-                var fromTop = $(this).scrollTop()+topMenuHeight;
+                var fromTop = $(this).scrollTop() + topMenuHeight;
 
                 // Get id of current scroll item
-                var cur = scrollItems.map(function(){
+                var cur = scrollItems.map(function () {
                     if ($(this).offset().top < fromTop)
                         return this;
                 });
                 // Get the id of the current element
-                cur = cur[cur.length-1];
+                cur = cur[cur.length - 1];
                 var id = cur && cur.length ? cur[0].id : "";
 
                 if (lastId !== id) {
@@ -244,13 +175,20 @@
                     // Set/remove active class
                     menuItems
                         .parent().removeClass("font-bold border-yellow-600")
-                        .end().filter("[href='#"+id+"']").parent().addClass("font-bold border-yellow-600");
+                        .end().filter("[href='#" + id + "']").parent().addClass("font-bold border-yellow-600");
                 }
             });
 
         </script>
 
-        </body>
-        </html>
+        <script src="<?= PATH ?>assets/js/wikis.js"></script>
+
+        <?php if (isset($_GET["wikis"])) { ?>
+            <script>
+              $("#home-section").hide();
+                $("#wikis-section").show();
+                $("#form-section").hide();
+            </script>
+        <?php } ?>
     </div>
 </div>
