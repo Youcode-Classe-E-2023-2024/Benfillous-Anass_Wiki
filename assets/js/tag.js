a5e5d6190970d35badc77b009591f16f00ad92ab
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:16:07 +0100

    ass tag insertion script using ajax

--- assets/js/tag.js
+++ assets/js/tag.js
@@ -4,3 +4,23 @@ const editTagInput = document.getElementById("edit-tag-input");
 const tagSection = document.getElementById("tag-section");
 let currentTagId;
 let currentTag;
+
+addTagBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (addTagInput.value.length !== 0) {
+        $.post(
+            "index.php?page=dashboard",
+            {
+                add_tag: true,
+                tag: addTagInput.value
+            },
+            (data) => {
+                console.log(data);
+                addTagInput.style.border = ""
+                addTagInput.value = "";
+                getTags();
+            }
+        )
+    } else
+        addTagInput.style.border = "1px solid red";
+})
