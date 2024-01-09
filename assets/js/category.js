16afbb938a742939937969dd684abd5e7bff3850
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:12:21 +0100

    create category script

---
+++ assets/js/category.js
@@ -0,0 +1,6 @@
+const addCategoryBtn = document.getElementById("addCategoryBtn");
+const addCategoryInput = document.getElementById("category-input");
+const editCategoryInput = document.getElementById("edit-category-input");
+const categorySection = document.getElementById("category-section");
+let currentCategoryId;
+let currentCategory;
