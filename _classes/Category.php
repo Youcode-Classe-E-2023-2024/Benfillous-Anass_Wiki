3b3448917342bf5c490a6be11633e8327072ee0d
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:08:26 +0100

    add category insertion method

--- _classes/Category.php
+++ _classes/Category.php
@@ -1,3 +1,10 @@
 <?php
 class Category {
+    function addCategory($category) {
+        global $db;
+        $sql = "INSERT INTO category (category) VALUES (:category)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':category', $category);
+        $stmt->execute();
+    }
 }
