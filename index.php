1a76dcd5601f839a08adeca07ddafe22600b0239
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 09:37:15 +0100

    check if the page wanted is dashboard to use its layout

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ spl_autoload_register(function ($class) {
     include_once '_classes/' . $class . '.php';
 });
 
-if (isset($_SESSION["login"])  && isset($_GET['page']) && ($_GET['page'] == "login" || $_GET['page'] == "register")) {
+if (isset($_SESSION["login"]) && isset($_GET['page']) && ($_GET['page'] == "login" || $_GET['page'] == "register")) {
     $page = 'home';
 } else if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
@@ -21,8 +21,10 @@ $all_pages = scandir('controllers');
 if (in_array($page . '_controller.php', $all_pages)) {
     include_once 'models/' . $page . '_model.php';
     include_once 'controllers/' . $page . '_controller.php';
-    /*include_once 'views/_layout.php';*/
-    include_once 'views/dashboard_layout.php';
+    if (isset($_SESSION["admin"]) && $page == "dashboard")
+        include_once 'views/dashboard_layout.php';
+    else
+        include_once 'views/_layout.php';
 } else {
     header('Location: 404.html');
 }
