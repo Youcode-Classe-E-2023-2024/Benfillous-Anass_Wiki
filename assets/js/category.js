cbed4a4b5f991a8797d55a0d742ee44d17282b05
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:13:31 +0100

    get categories using ajax

--- assets/js/category.js
+++ assets/js/category.js
@@ -25,3 +25,46 @@ addCategoryBtn.addEventListener("click", (event) => {
     } else
         addCategoryInput.style.border = "1px solid red";
 })
+
+function getCategories() {
+    categorySection.innerHTML = "";
+    $.get(
+        "index.php?page=dashboard&get_categories=true",
+        (data) => {
+            console.log(data);
+            let categories = JSON.parse(data);
+
+            categories.forEach((category) => {
+                categorySection.innerHTML += `<tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">${category.category_id}</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">${category.category}</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                <a href="#category-form" class="edit-category text-blue-600 dark:text-blue-500 hover:underline"
+                                                data-category-id="${category.category_id}" data-category-name="${category.category}">Edit</a>
+                                            </td>
+                                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                <a data-category-id="${category.category_id}" class="delete-category cursor-pointer text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                                            </td>
+                                        </tr>`;
+            })
+        }
+    )
+
+    $(categorySection).ready(() => {
+        $(categorySection).on("click", ".edit-category", function () {
+            currentCategoryId = $(this).data("category-id");
+            currentCategory = $(this).data("category-name");
+            console.log(document.querySelector("#edit-category-form").classList);
+            document.querySelector("#edit-category-form").classList.remove("hidden");
+            document.querySelector("#category-form").classList.add("hidden");
+            document.querySelector("#edit-category-input").value = currentCategory;
+        })
+
+        $(categorySection).on("click", ".delete-category", function () {
+            currentCategoryId = $(this).data("category-id");
+            deleteCategory(currentCategoryId);
+        });
+    })
+
+
+}
