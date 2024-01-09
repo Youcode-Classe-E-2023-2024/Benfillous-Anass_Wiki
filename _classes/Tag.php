de86dfe5b0f9e21c069a7167f325fd6fa205371a
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:09:52 +0100

    add tag insertion method

--- _classes/Tag.php
+++ _classes/Tag.php
@@ -1,3 +1,10 @@
 <?php
 class Tag {
+    function addTag($tag) {
+        global $db;
+        $sql = "INSERT INTO tag (tag) VALUES (:tag)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag', $tag);
+        $stmt->execute();
+    }
 }
