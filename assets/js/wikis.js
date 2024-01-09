d55603c3ba8e274a25243f2681b6cc3decc6ea57
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 00:22:20 +0100

    get all wikis script

--- assets/js/wikis.js
+++ assets/js/wikis.js
@@ -16,6 +16,7 @@ $(addWikiBtn).click(() => {
     $("#form-section").show();
 
 })
+
 $(wikiSubmitBtn).click(() => {
     createWiki();
     titleInput.value = "";
@@ -27,6 +28,20 @@ $(wikiSubmitBtn).click(() => {
     scrollToTop();
 })
 
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Use 'auto' for instant scrolling
+    });
+}
+
+//wiki inputs
+const titleInput = document.getElementById("title-input");
+const contentInput = document.getElementById("content-input");
+const tagInput = document.getElementById("tag-input");
+const categoryInput = document.getElementById("category-input");
+
+function createWiki() {
     $.post(
         "index.php?page=home",
         {
@@ -42,3 +57,33 @@ $(wikiSubmitBtn).click(() => {
     )
 }
 
+const wikisList = document.getElementById("wikis-list");
+
+function getWikis() {
+    wikisList.innerHTML = "";
+    $.get(
+        "index.php?page=home&getWikis=true",
+        (data) => {
+            let wikis = JSON.parse(data);
+
+            wikis.forEach((wiki) => {
+                wikisList.innerHTML += `<tr class="p-6">
+                    <td class="h-6 w-10 text-center rounded">${wiki.wiki_id}</td>
+                    <td class="h-6 w-32 text-center rounded">${wiki.title}</td>
+                    <td class="w-32 h-6 text-center rounded-lg">${wiki.content}</td>
+                    <td class="w-24 h-6 text-center rounded-lg">${wiki.creator}</td>
+                    <td class="w-24 h-6 text-center rounded-lg">tag</td>
+                    <td class="w-24 h-6 text-center rounded-lg">${wiki.category}</td>
+                    <td class="w-24 h-6 text-center rounded-lg">${wiki.created_date}</td>
+                    <td class="w-24 h-6 text-center rounded-lg">
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>`;
+
+            })
+        }
+    )
+}
+
+getWikis();
