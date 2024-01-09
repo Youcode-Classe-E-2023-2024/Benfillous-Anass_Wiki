187d9b4ee40e2d0233575d55cfd4b7bc4834c0ef
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:12:56 +0100

    add category insertion using ajax

--- assets/js/category.js
+++ assets/js/category.js
@@ -4,3 +4,24 @@ const editCategoryInput = document.getElementById("edit-category-input");
 const categorySection = document.getElementById("category-section");
 let currentCategoryId;
 let currentCategory;
+
+addCategoryBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("test");
+    if (addCategoryInput.value.length !== 0) {
+        $.post(
+            "index.php?page=dashboard",
+            {
+                add_category: true,
+                category: addCategoryInput.value
+            },
+            (data) => {
+                console.log(data);
+                addCategoryInput.style.border = ""
+                addCategoryInput.value = "";
+                getCategories();
+            }
+        )
+    } else
+        addCategoryInput.style.border = "1px solid red";
+})
