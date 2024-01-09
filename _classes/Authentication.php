69525cb7b9e28c217d82eba365fa7a6f2cae819c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 09:22:30 +0100

    add checker if the user is admin or not

--- _classes/Authentication.php
+++ _classes/Authentication.php
@@ -1,10 +1,12 @@
 <?php
 
 class Authentication {
-    function login ($user_id) {
+    function login ($user_id, $access) {
+        if($access === "dashboard")
+            $_SESSION["admin"] = true;
         $_SESSION["user_id"] = $user_id;
         $_SESSION["login"] = true;
-        header('Location: index.php?page=home');
+        header('Location: index.php?page=' . $access);
     }
 
     function logout () {
