73203993addfdbc9c76332cc8988d4f8992d4ed7
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:15:37 +0100

    add tag script

---
+++ assets/js/tag.js
@@ -0,0 +1,6 @@
+const addTagBtn = document.getElementById("addTagBtn");
+const addTagInput = document.getElementById("tag-input");
+const editTagInput = document.getElementById("edit-tag-input");
+const tagSection = document.getElementById("tag-section");
+let currentTagId;
+let currentTag;
