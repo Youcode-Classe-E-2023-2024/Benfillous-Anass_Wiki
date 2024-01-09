00ef303d783e646e9e0465313214387e2f54ae2b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 00:18:02 +0100

    add method for wiki insertion

--- _classes/Wiki.php
+++ _classes/Wiki.php
@@ -1,3 +1,19 @@
 <?php
 class Wiki {
+    function addWiki($title, $content, $tags, $category, $creator, $created_date) {
+        global $db;
+        $sql = "INSERT INTO wiki (title, content, category_id, creator, created_date) VALUES (:title, :content, :category, :creator, :created_date)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':category', $category);
+        $stmt->bindParam(':creator', $creator);
+        $stmt->bindParam(':created_date', $created_date);
+        $stmt->execute();
+        $wikiId = $db->lastInsertId();
+
+        foreach ($tags as $tag) {
+            Tag::wiki_tag($tag, $wikiId);
+        }
+    }
 }
