d94dd8cbd064df55b46a07de19dadec9e6ebd08b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 14:35:16 +0100

    show wiki creation form and hide wikis list

--- assets/js/wikis.js
+++ assets/js/wikis.js
@@ -1,8 +1,16 @@
 const manageWikisBtn = document.getElementById("manage-wikis-btn");
+const addWikiBtn = document.getElementById("add-wiki-btn");
 $("#wikis-section").hide();
 manageWikisBtn.addEventListener("click", () => {
     $("#home-section").hide();
     $("#wikis-section").show();
     $("#wikis-container").show();
     $("#form-section").hide();
+})
+
+
+$(addWikiBtn).click(()=> {
+    $("#wikis-container").hide();
+    $("#form-section").show();
+
 })
