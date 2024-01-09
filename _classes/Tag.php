48001b2b72ecf00a72bce56ae7c20a8b4d37a959
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-09 01:10:48 +0100

    add update tag method

--- _classes/Tag.php
+++ _classes/Tag.php
@@ -7,4 +7,13 @@ class Tag {
         $stmt->bindParam(':tag', $tag);
         $stmt->execute();
     }
+
+    function updateTag($tag, $tag_id) {
+        global $db;
+        $sql = "UPDATE tag SET tag = :tag WHERE tag_id = :tag_id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag', $tag);
+        $stmt->bindParam(':tag_id', $tag_id);
+        $stmt->execute();
+    }
 }
