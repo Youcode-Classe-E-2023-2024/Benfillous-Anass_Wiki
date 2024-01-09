4ddc0ea7135bacc82f06b0ea4873fa2d6624fed5
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 00:18:23 +0100

    add method for wiki update

--- _classes/Wiki.php
+++ _classes/Wiki.php
@@ -16,4 +16,21 @@ class Wiki {
             Tag::wiki_tag($tag, $wikiId);
         }
     }
+
+    function updateWiki($tags, $title, $content, $category, $creator, $updated_date) {
+        global $db;
+        $sql = "UPDATE wiki SET  title = :title, content = :content, category_id = :category_id, creator = :creator,  updated_date :updated_date WHERE tag_id = :tag_id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':category_id', $category);
+        $stmt->bindParam(':creator', $creator);
+        $stmt->bindParam(':updated_date', $updated_date);
+        $stmt->bindParam(':wiki_id', $wiki_id);
+        $stmt->execute();
+
+        foreach ($tags as $tag) {
+            Tag::update_wiki_tag($tag);
+        }
+    }
 }
