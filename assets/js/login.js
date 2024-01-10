86bf479bdd0c322e4b2a0b732a29206cf9262d81
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 14:21:37 +0100

    add login script

---
+++ assets/js/login.js
@@ -0,0 +1,41 @@
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    // login inputs
+    const loginForm = document.getElementById("login-form");
+    const spinnerWrapper = document.querySelector(".hourglassBackground"); // Added for the spinner wrapper
+    const loginEmail = document.getElementById("loginEmail").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+    $.ajax({
+        type: "POST",
+        url: "index.php?page=login",
+        data: {
+            req: "login",
+            email: loginEmail,
+            password: loginPassword
+        },
+        success: (response) => {
+            const data = JSON.parse(response);
+            console.log(data);
+            window.location.href = `index.php?page=${data.success}&success`;
+            if (data.success) {
+                console.log(data.success);
+                // loginForm.style.display = 'none';
+                // spinnerWrapper.style.display = 'block';
+                // setTimeout(() => {
+                //     window.location.href = 'index.php?page=home&success';
+                // }, 3000);
+
+            } else if (data.error) {
+                console.log(data.error);
+                Swal.fire({icon: "error", title: "Error", text: data.error});
+            }
+
+        }, error: (error) => {
+            console.log(error);
+
+        }
+    })
+})
