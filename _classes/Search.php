4f1e1b36d44268f8c4a35007439a6eccef61516c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 23:08:52 +0100

    add search for tags

--- _classes/Search.php
+++ _classes/Search.php
@@ -14,5 +14,21 @@ class Search
 
         return $res;
     }
+
+    static function searchForTags($tag)
+    {
+        global $db;
+        $tag = "%" . "$tag" . "%";
+        $sql = "SELECT tag.*, wiki.* FROM wiki_tag
+                JOIN tag ON wiki_tag.tag_id = tag.tag_id
+                JOIN wiki ON wiki_tag.wiki_id = wiki.wiki_id
+                WHERE tag LIKE :tag";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":tag", $tag, PDO::PARAM_STR);
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $res;
+    }
 }
 
