cc942c5c062272e0e700248bd3b82ff88a6762f9
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-10 14:20:49 +0100

    change login function to static

--- _classes/Authentication.php
+++ _classes/Authentication.php
@@ -1,12 +1,11 @@
 <?php
 
 class Authentication {
-    function login ($user_id, $access) {
+    static function login ($user_id, $access) {
         if($access === "dashboard")
             $_SESSION["admin"] = true;
         $_SESSION["user_id"] = $user_id;
         $_SESSION["login"] = true;
-        header('Location: index.php?page=' . $access);
     }
 
     function logout () {
