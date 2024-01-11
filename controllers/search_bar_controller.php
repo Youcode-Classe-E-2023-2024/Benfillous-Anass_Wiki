4e209a26e3fec65095ee41e40b03a7e45c562bd0
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-11 17:14:04 +0100

    update search controller

--- controllers/search_bar_controller.php
+++ controllers/search_bar_controller.php
@@ -1,17 +1,70 @@
 <?php
 
-if(isset($_GET["search_title"])) {
+if (isset($_GET["search_title"])) {
     $input_value = $_GET["input_value"];
     $searchedData = Search::searchForTitles($input_value);
+    $searchArray = [];
 
-    echo json_encode($searchedData);
+    foreach ($searchedData as $data) {
+        $wikiTags = Tag::get_wiki_tag($data["wiki_id"]);
+
+        $searchArray[] = [
+            "tags" => $wikiTags,
+            "wiki_infos" => $data
+        ];
+    }
+    echo json_encode($searchArray);
     exit;
 }
 
-if(isset($_GET["search_tag"])) {
+if (isset($_GET["search_tag"])) {
     $input_value = $_GET["input_value"];
     $searchedData = Search::searchForTags($input_value);
 
-    echo json_encode($searchedData);
+    $searchArray = [];
+    foreach ($searchedData as $data) {
+        $wikiTags = Tag::get_wiki_tag($data["wiki_id"]);
+
+        $searchArray[] = [
+            "tags" => $wikiTags,
+            "wiki_infos" => $data
+        ];
+    }
+    echo json_encode($searchArray);
+    exit;
+}
+
+if (isset($_GET["search_category"])) {
+    $input_value = $_GET["input_value"];
+    $searchedData = Search::searchForCategory($input_value);
+    $searchArray = [];
+
+    foreach ($searchedData as $data) {
+        $wikiTags = Tag::get_wiki_tag($data["wiki_id"]);
+
+        $searchArray[] = [
+            "tags" => $wikiTags,
+            "wiki_infos" => $data
+        ];
+    }
+    echo json_encode($searchArray);
+    exit;
+}
+
+if (isset($_GET["get_wiki_tags"])) {
+    $wiki_id = $_GET["wiki_id"];
+    $wikiTags = Tag::get_wiki_tag($wiki_id);
+
+    $searchArray = [];
+
+    foreach ($searchedData as $data) {
+        $wikiTags = Tag::get_wiki_tag($data["wiki_id"]);
+
+        $searchArray[] = [
+            "tags" => $wikiTags,
+            "wiki_infos" => $data
+        ];
+    }
+    echo json_encode($searchArray);
     exit;
 }
