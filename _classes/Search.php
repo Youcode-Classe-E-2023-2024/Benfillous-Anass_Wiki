8e393f0b1491d634c696323bba3bbbd07dc6cdfb
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-12 15:07:03 +0100

    update search class

--- _classes/Search.php
+++ _classes/Search.php
@@ -6,8 +6,9 @@ class Search
     {
         global $db;
         $title = "%" . "$title" . "%";
-        $sql = "SELECT wiki.*, category.*
+        $sql = "SELECT wiki.*, category.*, users.username
                 FROM wiki
+                JOIN users ON wiki.creator = users.user_id
                 JOIN category ON wiki.category_id = category.category_id
                 WHERE title LIKE :title AND archived = 0";
         $stmt = $db->prepare($sql);
@@ -22,9 +23,10 @@ class Search
     {
         global $db;
         $tag = "%" . "$tag" . "%";
-        $sql = "SELECT DISTINCT wiki.*, category.*
+        $sql = "SELECT DISTINCT wiki.*, category.*, users.username
                 FROM wiki_tag
                          JOIN wiki ON wiki_tag.wiki_id = wiki.wiki_id
+                        JOIN users ON wiki.creator = users.user_id
                          JOIN tag ON wiki_tag.tag_id = tag.tag_id
                          JOIN category ON wiki.category_id = category.category_id
                 WHERE tag.tag LIKE :tag AND archived = 0";
@@ -40,7 +42,8 @@ class Search
     {
         global $db;
         $category = "%" . "$category" . "%";
-        $sql = "SELECT category.*, wiki.* FROM wiki
+        $sql = "SELECT category.*, wiki.*, users.username FROM wiki
+                JOIN users ON wiki.creator = users.user_id
                 JOIN category ON wiki.category_id = category.category_id
                 WHERE category.category LIKE :category AND archived=0";
         $stmt = $db->prepare($sql);
