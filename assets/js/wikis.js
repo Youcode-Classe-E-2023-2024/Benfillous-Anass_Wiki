5a732b85980fede423d7c195dc14d18ad2835c59
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-13 23:45:26 +0100

    update wiki script

--- assets/js/wikis.js
+++ assets/js/wikis.js
@@ -4,13 +4,6 @@ const wikiSubmitBtn = document.getElementById("wiki-submit");
 const wikiSection = document.getElementById("wikis-section");
 const editWikiSubmitBtn = document.getElementById("edit-wiki-submit");
 
-$("#wikis-section").hide();
-manageWikisBtn.addEventListener("click", () => {
-    $("#home-section").hide();
-    $("#wikis-section").show();
-    $("#wikis-container").show();
-    $("#form-section").hide();
-})
 
 $(addWikiBtn).click(() => {
     $("#wikis-container").hide();
@@ -218,3 +211,23 @@ function archiveWiki() {
 }
 
 getWikis();
+
+function formatTimestamp(timestamp) {
+    // Convert timestamp to milliseconds
+    const date = new Date(timestamp * 1000);
+
+    // Extract components
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Months are zero-based
+    const year = date.getFullYear();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    // Check if it's a full date or just time
+    if (day !== 1 || month !== 1 || year !== 1970) {
+        return `${day}/${month < 10 ? '0' : ''}${month}/${year}, ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    } else {
+        return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    }
+}
