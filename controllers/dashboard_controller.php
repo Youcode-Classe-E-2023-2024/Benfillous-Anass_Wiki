d253ef30ce76c408772dc36aad1e59a83996b93e
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-13 23:46:13 +0100

    update dashboard controller

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,30 +1,30 @@
 <?php
 
-if(empty($_SESSION["admin"])) {
+if (empty($_SESSION["admin"])) {
     header("Location: index.php");
 }
 
-if(isset($_POST["add_category"])) {
+if (isset($_POST["add_category"])) {
     $category = filter_input(INPUT_POST, "category", FILTER_SANITIZE_SPECIAL_CHARS);
     $categoryObj->addCategory($category);
     echo "category added successfully";
     exit;
 }
 
-if(isset($_GET["get_categories"])) {
+if (isset($_GET["get_categories"])) {
     $categories = $categoryObj->getCategories();
     echo json_encode($categories);
     exit;
 }
 
-if(isset($_POST["delete_category"])) {
+if (isset($_POST["delete_category"])) {
     $categoryId = filter_input(INPUT_POST, "category_id", FILTER_SANITIZE_SPECIAL_CHARS);
     $categoryObj->deleteCategory($categoryId);
     echo "category deleted successfully";
     exit;
 }
 
-if(isset($_POST["edit_category"])) {
+if (isset($_POST["edit_category"])) {
     $category = filter_input(INPUT_POST, "category", FILTER_SANITIZE_SPECIAL_CHARS);
     $categoryId = filter_input(INPUT_POST, "category_id", FILTER_SANITIZE_SPECIAL_CHARS);
     $categoryObj->updateCategory($category, $categoryId);
@@ -32,27 +32,27 @@ if(isset($_POST["edit_category"])) {
     exit;
 }
 
-if(isset($_POST["add_tag"])) {
+if (isset($_POST["add_tag"])) {
     $tag = filter_input(INPUT_POST, "tag", FILTER_SANITIZE_SPECIAL_CHARS);
     $tagObj->addTag($tag);
     echo "tag added successfully";
     exit;
 }
 
-if(isset($_GET["get_tags"])) {
+if (isset($_GET["get_tags"])) {
     $tags = $tagObj->getTags();
     echo json_encode($tags);
     exit;
 }
 
-if(isset($_POST["delete_tag"])) {
+if (isset($_POST["delete_tag"])) {
     $tagId = filter_input(INPUT_POST, "tag_id", FILTER_SANITIZE_SPECIAL_CHARS);
     $tagObj->deleteTag($tagId);
     echo "tag deleted successfully";
     exit;
 }
 
-if(isset($_POST["edit_tag"])) {
+if (isset($_POST["edit_tag"])) {
     $tag = filter_input(INPUT_POST, "tag", FILTER_SANITIZE_SPECIAL_CHARS);
     $tagId = filter_input(INPUT_POST, "tag_id", FILTER_SANITIZE_SPECIAL_CHARS);
     $tagObj->updateTag($tag, $tagId);
@@ -68,7 +68,7 @@ if (isset($_GET["getArchivedWikis"])) {
     exit;
 }
 
-if(isset($_POST["restore"])) {
+if (isset($_POST["restore"])) {
     $wiki_id = $_POST["wikiId"];
 
     $wikiObj->restoreWiki($wiki_id);
@@ -76,3 +76,13 @@ if(isset($_POST["restore"])) {
     echo "wiki restored successfully";
     exit;
 }
+
+
+$wikisNumber = count($wikiObj->getWikis());
+$categoriesNumber = count($categoryObj->getCategories());
+$tagsNumber = count($tagObj->getTags());
+$usersNumber = count(User::getAll());
+
+$users = array_slice(User::getAll(), 0, 10);
+
+$admin = new User($_SESSION["user_id"]);
